73f10c7fc1a5338dacf714d06fae1cf14d84fd9a
micymike <mosesmichael878@gmail.com>
2024-11-30 15:42:19 +0300

    port

--- app.py
+++ app.py
@@ -219,4 +219,5 @@ def get_similar_profiles():
         return jsonify({'error': 'Internal server error'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=True)
