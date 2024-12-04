676e62e007d168a0dfa5a948521061988a8717f2
micymike <mosesmichael878@gmail.com>
2024-12-04 17:05:20 +0300

    cors issue solved

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 # CORS Configuration
 CORS(app, 
      resources={r"/*": {
-         "origins": "http://localhost:3000",
+         "origins": ["http://localhost:3000", "https://skill3-frontend-1.onrender.com"],
          "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
          "allow_headers": ["Content-Type", "Authorization"],
          "supports_credentials": True
